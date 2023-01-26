59bdd01632852dea216b366d29dbfe3d17a31dbf
Marcelo <nesdimu@gmail.com>
2023-01-26 14:22:01 -0300

    comienza el CV

--- js/funciones.js
+++ js/funciones.js
@@ -4,23 +4,26 @@ document.getElementById("slidefotos").addEventListener('click', function () {
     document.getElementById('carouselfotos').style.display='block';
     document.getElementById('carouselvideos').style.display='none';
     document.getElementById('cv').style.display='none';
+    document.getElementById('carouselfotos').scrollIntoView();
 });
 
 document.getElementById("slidevideos").addEventListener('click', function () {
     document.getElementById('carouselfotos').style.display='none';
     document.getElementById('carouselvideos').style.display='block';
     document.getElementById('cv').style.display='none';
+    document.getElementById('carouselvideos').scrollIntoView();
 });
 
 document.getElementById("curriculum").addEventListener('click', function () {
     document.getElementById('carouselfotos').style.display='none';
     document.getElementById('carouselvideos').style.display='none';
     document.getElementById('cv').style.display='block';
+    document.getElementById('cv').scrollIntoView();
 });
 
 // boton go-top
 window.onscroll = function(){
-    if (document.documentElement.scrollTop >80){
+    if (document.documentElement.scrollTop >200){
         document.querySelector('.go-top-container').classList.add('show'); 
     }else{
         document.querySelector('.go-top-container').classList.remove('show'); 
@@ -31,4 +34,5 @@ document.querySelector('.go-top-container').addEventListener('click', () =>{
     top:0,
     behavior:"smooth"
     });
+
 });
