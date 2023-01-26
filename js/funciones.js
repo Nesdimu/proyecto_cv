5cef3564f912d264c4dc772cb6b4eeb9ef22bc6c
Marcelo <nesdimu@gmail.com>
2023-01-26 20:06:12 -0300

    correcciones de estilo

--- js/funciones.js
+++ js/funciones.js
@@ -1,38 +1,58 @@
 
 // oculta y muestra secciones
+// document.getElementById("slidefotos").addEventListener('click', function () {
+//     document.getElementById('carouselfotos').style.display = 'block';
+//     document.getElementById('carouselvideos').style.display = 'none';
+//     document.getElementById('cv').style.display = 'none';
+//     document.getElementById('carouselfotos').scrollIntoView();
+// });
+
+// document.getElementById("slidevideos").addEventListener('click', function () {
+//     document.getElementById('carouselfotos').style.display = 'none';
+//     document.getElementById('carouselvideos').style.display = 'block';
+//     document.getElementById('cv').style.display = 'none';
+//     document.getElementById('carouselvideos').scrollIntoView();
+// });
+
+// document.getElementById("curriculum").addEventListener('click', function () {
+//     document.getElementById('carouselfotos').style.display = 'none';
+//     document.getElementById('carouselvideos').style.display = 'none';
+//     document.getElementById('cv').style.display = 'block';
+//     document.getElementById('cv').scrollIntoView();
+// });
+
 document.getElementById("slidefotos").addEventListener('click', function () {
-    document.getElementById('carouselfotos').style.display='block';
-    document.getElementById('carouselvideos').style.display='none';
-    document.getElementById('cv').style.display='none';
-    document.getElementById('carouselfotos').scrollIntoView();
+    show('carouselfotos', 'carouselvideos', 'cv');
 });
 
 document.getElementById("slidevideos").addEventListener('click', function () {
-    document.getElementById('carouselfotos').style.display='none';
-    document.getElementById('carouselvideos').style.display='block';
-    document.getElementById('cv').style.display='none';
-    document.getElementById('carouselvideos').scrollIntoView();
+    show('carouselvideos', 'carouselfotos', 'cv');
 });
 
 document.getElementById("curriculum").addEventListener('click', function () {
-    document.getElementById('carouselfotos').style.display='none';
-    document.getElementById('carouselvideos').style.display='none';
-    document.getElementById('cv').style.display='block';
-    document.getElementById('cv').scrollIntoView();
+    show('cv', 'carouselvideos','carouselfotos');
 });
 
+function show(idToShow, idToHide1, idToHide2) {
+    document.getElementById(idToShow).style.display = 'block';
+    document.getElementById(idToHide1).style.display = 'none';
+    document.getElementById(idToHide2).style.display = 'none';
+    document.getElementById(idToShow).scrollIntoView();
+}
+
+
 // boton go-top
-window.onscroll = function(){
-    if (document.documentElement.scrollTop >200){
-        document.querySelector('.go-top-container').classList.add('show'); 
-    }else{
-        document.querySelector('.go-top-container').classList.remove('show'); 
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        document.querySelector('.go-top-container').classList.add('show');
+    } else {
+        document.querySelector('.go-top-container').classList.remove('show');
     }
 }
-document.querySelector('.go-top-container').addEventListener('click', () =>{
+document.querySelector('.go-top-container').addEventListener('click', function () {
     window.scrollTo({
-    top:0,
-    behavior:"smooth"
+        top: 0,
+        behavior: "smooth"
     });
 
 });
