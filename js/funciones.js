5e8c71e55c5d1176e2c424a63e6cea864db896a0
Marcelo <nesdimu@gmail.com>
2023-01-27 17:32:15 -0300

    cambios en visualizacion CV animado en vertical centrado

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,62 @@
 
+//elementos del cv
+document.getElementById('objetivos').addEventListener('mouseenter', function () {
+    showCV('objetivosInfo', ['formacionInfo', 'experienciaInfo', 'exposicionesInfo', 'equipoInfo', 'herramientasInfo']);
+    activarIcono('objetivos', ['formacion', 'experiencia', 'exposiciones', 'equipo', 'herramientas']);
+});
 
-// oculta y muestra secciones
+document.getElementById('formacion').addEventListener('mouseenter', function () {
+    showCV('formacionInfo', ['objetivosInfo', 'experienciaInfo', 'exposicionesInfo', 'equipoInfo', 'herramientasInfo']);
+    activarIcono('formacion', ['objetivos', 'experiencia', 'exposiciones', 'equipo', 'herramientas']);
+});
+
+document.getElementById('experiencia').addEventListener('mouseenter', function () {
+    showCV('experienciaInfo', ['objetivosInfo', 'formacionInfo', 'exposicionesInfo', 'equipoInfo', 'herramientasInfo']);
+    activarIcono('experiencia', ['objetivos', 'formacion', 'exposiciones', 'equipo', 'herramientas']);
+});
+
+document.getElementById('exposiciones').addEventListener('mouseenter', function () {
+    showCV('exposicionesInfo', ['objetivosInfo', 'formacionInfo', 'experienciaInfo', 'equipoInfo', 'herramientasInfo']);
+    activarIcono('exposiciones', ['objetivos', 'experiencia', 'formacion', 'equipo', 'herramientas']);
+});
+
+
+document.getElementById('equipo').addEventListener('mouseenter', function () {
+    showCV('equipoInfo', ['objetivosInfo', 'formacionInfo', 'exposicionesInfo', 'experienciaInfo', 'herramientasInfo']);
+    activarIcono('equipo', ['objetivos', 'experiencia', 'exposiciones', 'formacion', 'herramientas']);
+});
+
+
+document.getElementById('herramientas').addEventListener('mouseenter', function () {
+    showCV('herramientasInfo', ['objetivosInfo', 'formacionInfo', 'exposicionesInfo', 'equipoInfo', 'experienciaInfo']);
+    activarIcono('herramientas', ['objetivos', 'experiencia', 'exposiciones', 'equipo', 'formacion']);
+});
+
+
+
+function showCV(idToShowInfo, idsToHideInfo) {
+    x = 0;
+    for (x; x <= 4; x++) {
+        document.getElementById((idsToHideInfo)[x]).style.display = 'none';
+    }
+    document.getElementById(idToShowInfo).style.display = 'block';
+
+};
+
+function activarIcono(iconoOn, iconosOff) {
+    x = 0;
+    for (x; x <= 4; x++) {
+        document.getElementById((iconosOff)[x]).classList.remove('icono-activo');
+    }
+    console.log("clic en icono antes");
+    document.getElementById(iconoOn).classList.add('icono-activo');
+    console.log("clic en icono despues");
+};
+
+//************************************************************************************** */
+
+
+// oculta y muestra secciones - agrega clase a la seccion activa
 document.getElementById("slidefotos").addEventListener('click', function () {
     show('carouselfotos', 'carouselvideos', 'cv', 'slidefotos', 'slidevideos', 'curriculum');
 });
@@ -21,8 +77,8 @@ function show(idToShow, idToHide1, idToHide2, seccionSi, seccionNo1, seccionNo2)
     document.getElementById(seccionSi).classList.add('seccion-activa');
     document.getElementById(seccionNo1).classList.remove('seccion-activa');
     document.getElementById(seccionNo2).classList.remove('seccion-activa');
-}
-
+};
+//************************************************************************************** */
 
 // boton go-top
 window.onscroll = function () {
@@ -31,7 +87,7 @@ window.onscroll = function () {
     } else {
         document.querySelector('.go-top-container').classList.remove('show');
     }
-}
+};
 document.querySelector('.go-top-container').addEventListener('click', function () {
     window.scrollTo({
         top: 0,
