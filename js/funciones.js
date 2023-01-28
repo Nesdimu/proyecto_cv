70b85554ec2c2df96e236728e7f517bb1ae425e3
Marcelo <nesdimu@gmail.com>
2023-01-28 11:15:58 -0300

    correccion de movimiento en secciones y ancho del cv

--- js/funciones.js
+++ js/funciones.js
@@ -76,7 +76,7 @@ function show(idToShow, idToHide1, idToHide2, seccionSi, seccionNo1, seccionNo2)
     document.getElementById(idToShow).style.display = 'block';
     document.getElementById(idToHide1).style.display = 'none';
     document.getElementById(idToHide2).style.display = 'none';
-    document.getElementById(idToShow).scrollIntoView();
+    // document.getElementById(idToShow).scrollIntoView();
     document.getElementById(seccionSi).classList.add('seccion-activa');
     document.getElementById(seccionNo1).classList.remove('seccion-activa');
     document.getElementById(seccionNo2).classList.remove('seccion-activa');
